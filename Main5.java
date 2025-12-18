b631e48bef863296e6f438c3920b445997009ab9
Anilmrnzd <Anilmirinezhad@gmail.com>
2025-12-18 20:19:59 +0330

    Print my name~
    My name is Anil and I want Java to print my name with ASCII ART

---
+++ Main5.java
@@ -0,0 +1,18 @@
+public class Main5 {
+
+        public static void main(String[] args) {
+
+            System.out.println("        AAAA          NNNN          NN   IIIIIIII    LL");
+            System.out.println("      AA    AA        NN  NN        NN      II       LL");
+            System.out.println("     AA       AA      NN    NN      NN      II       LL");
+            System.out.println("    AA         AA     NN     NN     NN      II       LL");
+            System.out.println("   AA           AA    NN      NN    NN      II       LL");
+            System.out.println("  AAAAAAAAAAAAAAAA    NN       NN   NN      II       LL");
+            System.out.println("  AA             AA   NN        NN  NN      II       LL");
+            System.out.println("  AA             AA   NN         NN NN      II       LL");
+            System.out.println("  AA             AA   NN          NNN    IIIIIIII    LLLLLLLLLL");
+
+        }
+    }
+
+
